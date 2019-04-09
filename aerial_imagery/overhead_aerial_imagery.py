73e0ed6d968c302866cbbd57c3dac559e82c05e5
avi.thaker@gmail.com <avi.thaker@gmail.com>
2019-04-09 03:29:04 -0400

    functional api

--- aerial_imagery/overhead_aerial_imagery.py
+++ aerial_imagery/overhead_aerial_imagery.py
@@ -20,6 +20,7 @@ import os
 from tensorflow.keras.preprocessing.image import ImageDataGenerator, array_to_img, img_to_array, load_img
 import pandas as pd
 import datetime
+import numpy as np
 
 def fuzzy_search(input_df, search_date, days, search_col):
     """ Finds the percent change and the direction for a given input
@@ -61,8 +62,10 @@ df["Date"] = pd.to_datetime(df["name"].str.split("_").apply(lambda x: x[0]))
 # Read in the images one-by-one
 img_arrs = []
 for i in range(len(only_files)):
-    img_arr = img_to_array(load_img(os.path.join(data_dir, only_files[i]), target_size=(224,224))) / 255
+    img_arr = img_to_array(load_img(os.path.join(data_dir, only_files[i]), target_size=(64,64))) / 255
     img_arrs.append(img_arr)
+
+img_arrs = np.array(img_arrs)
 
 # Load up a pandas dataframe of the stock data
 fut_data = pd.read_csv("futures_data/CHRIS-CME_W1.csv", sep=",")
@@ -71,12 +74,12 @@ fut_data.set_index("Date", inplace=True)
 fut_data.sort_index(inplace=True)
 
 # Compute the return after some amount of days
-n_days = [-180, -60, -30, -14, -7, -1, 1, 7, 14, 30, 60, 180]
-n_days = [-20, 20]
+n_days = [-100, -60, -30, -14, -7, -1, 1, 7, 14, 30, 60, 180]
+
 
 for duration in n_days:
-    col_name_pct = "pct_before_" + str(duration) if duration>0 else "pct_after_" + str(duration)
-    col_name_direction = "direction_before_" + str(duration) if duration > 0 else "direction_after_" + str(duration)
+    col_name_pct = "pct_after_" + str(abs(duration)) if duration>0 else "pct_before_" + str(abs(duration))
+    col_name_direction = "direction_after_" + str(abs(duration)) if duration > 0 else "direction_before_" + str(abs(duration))
     col_pct = []
     col_direction = []
     for index, row in df.iterrows():
@@ -89,8 +92,75 @@ for duration in n_days:
 
 # now build the keras model that can take in a conv2d input
 
+from tensorflow.keras.models import Model
+from tensorflow.keras.layers import Input, Embedding, LSTM, Dense
+from tensorflow.keras.layers import Conv2D, MaxPooling2D, Dropout, Flatten, Softmax
+from tensorflow.keras.optimizers import RMSprop, Adam, SGD
+from tensorflow.keras.layers import Concatenate, Average
 
 
+
+
+
+picture_input = Input(shape=(64,64,3), dtype="float32", name="picture_input")
+ts_input =      Input(shape=(2,), dtype="float32", name="ts_input")
+ddd = Dense(10)(ts_input)
+
+hidden_1 = Conv2D(32, (3,3), activation="relu")(picture_input)
+hidden_2 = MaxPooling2D(pool_size=(2,2))(hidden_1)
+hidden_3 = Dropout(0.25)(hidden_2)
+flatten_layer = Flatten()(hidden_3)
+dd = Dense(10)(flatten_layer)
+
+
+merged = Average()([dd, ddd])
+dense = Dense(1)(merged)
+model = Model(inputs=[picture_input, ts_input], outputs=dense)
+
+sgd = SGD(lr=0.01, momentum=0.9, nesterov=True)
+model.compile(loss='mean_squared_error', metrics=['accuracy', 'mae'], optimizer=sgd)
+# binary_crossentropy
+
+hist = model.fit([img_arrs, df[["direction_before_14", "direction_before_30"]].values], np.array(df["direction_after_30"].to_list()), epochs=50, verbose=1, validation_split=0.2)
+
+#from tensorflow.keras.utils import plot_model
+
+#plot_model(model)
+import matplotlib.pyplot as plt
+
+"""
+
+model.add(Conv2D(32, (3, 3), activation='relu', input_shape=(100, 100, 3)))
+model.add(Conv2D(32, (3, 3), activation='relu'))df.direction_before_20.to_list()
+model.add(MaxPooling2D(pool_size=(2, 2)))
+model.add(Dropout(0.25))
+
+model = Sequential()
+model.add(Conv2D(32, (3, 3), padding='same',
+                 input_shape=(32,32,3)))
+model.add(Activation('relu'))
+model.add(Conv2D(32, (3, 3)))
+model.add(Activation('relu'))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+model.add(Dropout(0.25))
+model.add(Conv2D(64, (3, 3), padding='same'))
+model.add(Activation('relu'))
+model.add(Conv2D(64, (3, 3)))
+model.add(Activation('relu'))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+model.add(Dropout(0.25))
+model.add(Flatten())
+model.add(Dense(512))
+model.add(Activation('relu'))
+model.add(Dropout(0.5))
+model.add(Dense(10, activation='softmax'))
+model.compile(optimizers.rmsprop(lr=0.0001, decay=1e-6),loss="categorical_crossentropy",metrics=["accuracy"])
+
+
+
+ts_input = Input(shape=)
+
+"""
 
 #datagen = ImageDataGenerator(rescale=1./255)
 #data_generator = datagen.flow_from_directory(
